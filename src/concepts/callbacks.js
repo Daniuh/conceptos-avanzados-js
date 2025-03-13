79803ebc47e6f48627b4360e4464c575b70d5c7c
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-13 12:11:07 +0100

    Se trabaja con los calbaks hell

--- src/concepts/callbacks.js
+++ src/concepts/callbacks.js
@@ -6,16 +6,28 @@ import { heroes } from '../data/heroes'
  */
 export const callbacksComponent = (element) => {
     
-    const id = '5d86371f2343e37870b91ef1';
-    findHero(id, (error, hero) => {
+    const id1 = '5d86371f2343e37870b91ef1';
+    const id2 = '5d86371f25a058e5b1c8a65e';
+
+    findHero(id1, (error, hero1) => {
         //element.innerHTML = hero?.name || 'No hay heroe'; Forma de medio validar o retornar correctamente un error
 
         if (error) {
             element.innerHTML = error;
+            return;
         }
 
-        element.innerHTML = hero.name;
-    });
+    findHero(id2, (error, hero2) => {
+        //element.innerHTML = hero?.name || 'No hay heroe'; Forma de medio validar o retornar correctamente un error
+
+        if (error) {
+            element.innerHTML = error;
+            return;
+        }
+
+        element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    })
+})
 }
 
 /**
