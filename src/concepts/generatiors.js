82590f32f336dbe572ab62167bf3ac24f87ee595
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-20 08:40:52 +0100

    Se crea un ejemplo de uso para las function* generando ids o mejor entendido un contador

--- src/concepts/generatiors.js
+++ src/concepts/generatiors.js
@@ -4,20 +4,41 @@
  */
 export const generatiorsFunctions = (element) => {
 
-    const myGen = myFirstGenerationFunction();
+    /* const myGen = myFirstGenerationFunction();
 
     console.log(myGen.next());
     console.log(myGen.next());
     console.log(myGen.next());
     console.log(myGen.next());
-    console.log(myGen.next());
+    console.log(myGen.next()); */
+
+    const genId = idGenerator();
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+    const renderButton = () => {
+        const {value} = genId.next();
+        button.innerText = `Click ${value}`
+    }
+
+    button.addEventListener('click', renderButton);
 }
 
-function* myFirstGenerationFunction() {
+/* function* myFirstGenerationFunction() {
     yield 'Primer valor';
     yield 'Segundo valor';
     yield 'Tercer valor';
     yield 'Cuarto valor';
 
     return 'Ya no hay valores';
+} */
+
+function* idGenerator() {
+    let currentId = 0;
+
+    while (true) {
+        yield ++currentId;
+    }
 }
